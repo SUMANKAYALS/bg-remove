ad3a6d5a39adacae11b9bf54df2fe36e01ea8f04
Suman Kayal <suman13kayal@gmail.com>
2025-12-27 01:20:13 +0530

    frontend url update

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 // app.use(cors());
 app.use(
     cors({
-        origin: "http://localhost:5173", // frontend URL
+        origin: "http://localhost:5173" || "bg-remove-pink.vercel.app", // frontend URL
         credentials: true,
         allowedHeaders: ["Content-Type", "Authorization"],
         methods: ["GET", "POST", "PUT", "DELETE"],
